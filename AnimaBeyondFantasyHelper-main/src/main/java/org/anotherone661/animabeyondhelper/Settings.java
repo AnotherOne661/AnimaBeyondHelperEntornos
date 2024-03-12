840e08a1fa58e30d68939ca70f27f11df3a818b4
Another <159118983+AnotherOne661@users.noreply.github.com>
2024-03-12 09:11:50 +0100

    Cuarto commit Settingsjava

--- AnimaBeyondFantasyHelper-main/src/main/java/org/anotherone661/animabeyondhelper/Settings.java
+++ AnimaBeyondFantasyHelper-main/src/main/java/org/anotherone661/animabeyondhelper/Settings.java
@@ -11,9 +11,8 @@ public class Settings {
     public static final boolean WINDOW_RESIZABLE = false;
     public static final Dimension WINDOW_SIZE = new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT);
     public static final boolean ALWAYS_ON_TOP = true;
-
+    //TODO Actual buttons
     public static final String ICON_PATH = "/AnimaLogo.png";
     public static final String BG_PATH = "/bg.jpg";
 }
 
-¡
